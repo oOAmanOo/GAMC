c7b53d3c682851af29295dab91f98cc8cc8a816d
oOAmanOo <iamamanda021024@gmail.com>
2024-11-14 15:26:28 +0800

    fix: correct structure

--- Main/test.py
+++ Main/test.py
@@ -43,23 +43,31 @@ class self_multi(nn.Module):
         # self attention
         self.selfAttentionMultihead = nn.MultiheadAttention(768, 1)
         self.selfAttentionLayerNorm = nn.LayerNorm(768, eps=eps)
-        self.selfAttentionLinear = nn.Linear(768, 768)
+        self.selfAttentionLinear1 = nn.Linear(768, 768)
+        self.selfAttentionRelu = nn.ReLU()
+        self.selfAttentionLinear2 = nn.Linear(768, 768)
         self.selfAttentionLayerNorm2 = nn.LayerNorm(768, eps=eps)
 
         # multihead attention
         self.multiheadAttentionMultihead = nn.MultiheadAttention(768, 8)
-        self.multiheadAttentionLinear = nn.Linear(768, 768)
+        self.multiheadAttentionLinear1 = nn.Linear(768, 768)
+        self.multiheadAttentionRelu = nn.ReLU()
+        self.multiheadAttentionLinear2 = nn.Linear(768, 768)
         self.multiheadAttentionLayerNorm = nn.LayerNorm(768, eps=eps)
 
     def forward(self, image, text):
         # self attention module
         self_out = self.selfAttentionMultihead(image, image, image)[0]
-        self_out = self.selfAttentionLinear(self_out)
+        self_out = self.selfAttentionLinear1(self_out)
+        self_out = self.selfAttentionRelu(self_out)
+        self_out = self.selfAttentionLinear2(self_out)
         self_out = self.selfAttentionLayerNorm(self_out + image)
 
         # multihead attention module
         multi_out = self.multiheadAttentionMultihead(text, text, text)[0]
-        multi_out = self.multiheadAttentionLinear(multi_out)
+        multi_out = self.multiheadAttentionLinear1(multi_out)
+        multi_out = self.multiheadAttentionRelu(multi_out)
+        multi_out = self.multiheadAttentionLinear2(multi_out)
         multi_out = self.multiheadAttentionLayerNorm(multi_out + text)
 
         return self_out, multi_out
@@ -70,23 +78,31 @@ class co_attention(nn.Module):
         super(co_attention, self).__init__()
         # co-attention text
         self.coAttentionTextMultihead = nn.MultiheadAttention(768, 1)
-        self.coAttentionTextLinear = nn.Linear(768, 768)
+        self.coAttentionTextLinear1 = nn.Linear(768, 768)
+        self.coAttentionTextRelu = nn.ReLU()
+        self.coAttentionTextLinear2 = nn.Linear(768, 768)
         self.coAttentionTextLayerNorm = nn.LayerNorm(768, eps=eps)
 
         # co-attention image
         self.coAttentionImageMultihead = nn.MultiheadAttention(768, 1)
-        self.coAttentionImageLinear = nn.Linear(768, 768)
+        self.coAttentionImageLinear1 = nn.Linear(768, 768)
+        self.coAttentionImageRelu = nn.ReLU()
+        self.coAttentionImageLinear2 = nn.Linear(768, 768)
         self.coAttentionImageLayerNorm = nn.LayerNorm(768, eps=eps)
 
     def forward(self, image, text):
         # co-attention image module
         visual_attending_textual = self.coAttentionTextMultihead(image, text, text)[0]
-        visual_attending_textual = self.coAttentionTextLinear(visual_attending_textual)
+        visual_attending_textual = self.coAttentionTextLinear1(visual_attending_textual)
+        visual_attending_textual = self.coAttentionTextRelu(visual_attending_textual)
+        visual_attending_textual = self.coAttentionTextLinear2(visual_attending_textual)
         visual_attending_textual = self.coAttentionTextLayerNorm(visual_attending_textual + image)
 
         # co-attention text module
         textual_attending_visual = self.coAttentionTextMultihead(text, image, image)[0]
-        textual_attending_visual = self.coAttentionTextLinear(textual_attending_visual)
+        textual_attending_visual = self.coAttentionTextLinear1(textual_attending_visual)
+        textual_attending_visual = self.coAttentionTextRelu(textual_attending_visual)
+        textual_attending_visual = self.coAttentionTextLinear2(textual_attending_visual)
         textual_attending_visual = self.coAttentionTextLayerNorm(textual_attending_visual + text)
 
         return visual_attending_textual, textual_attending_visual
@@ -124,13 +140,14 @@ class Generator(nn.Module):
 
             # get max value of each row, total 32*64
             top_k_values, top_k_indices = torch.topk(x2, 1, dim=2, largest=True)
-            toGemma = textExtractReverse(gemma, tokenizer,top_k_indices).to(device)
+            output_text = textExtractReverse(gemma, tokenizer, gemmaConfig, top_k_indices).to(device)
+            # toGemma = textExtractReverse(gemma, tokenizer, top_k_indices, Test=True).to(device)
             # 使用gemma作為model的一部分
-            output = self.gemma(toGemma)
+            # output = self.gemma(toGemma)
             # output[0] = last_hidden_state
             # output[1] = past_key_values
 
-        return output[0]
+        return output_text
 
     def forward(self, text, image):
         # max_seq_len = max(text.shape[1], image.shape[1])
@@ -167,11 +184,11 @@ class Generator(nn.Module):
 
         return output_text, output_funny_score
 
-    def generate(self, image, max_length=100):
+    def generate(self, text, image):
         generated_tokens = []
         generated_tokens.append(2)  # <bos> = 2
         text = torch.zeros_like(image).to(device)
-        text = text.transpose(0, 1)
+        text = text.transpose(0, 1).to(torch.bfloat16)
         image = image.transpose(0, 1)
         tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b-it")
         depth = len(self.layers_self_multi)
@@ -182,51 +199,53 @@ class Generator(nn.Module):
             zeros[::2] = list
             return zeros
 
-        lastTurn = False
         with torch.no_grad():
-            for _ in range(max_length + 1):
-                # Transformer
-                for i in range(depth):
-                    # self attention
-                    image, text = self.layers_self_multi[i](image, text)
-                    # co-attention
-                    image, text = self.layers_co_attention[i](image, text)
+            # Transformer
+            for i in range(depth):
+                # self attention
+                image, text = self.layers_self_multi[i](image, text)
+                # co-attention
+                image, text = self.layers_co_attention[i](image, text)
+
+            # feature fusion
+            feature_fusion = image + text  # visual_attending_textual + textual_attending_visual
+            feature_fusionFF = self.feedForwardLinear(feature_fusion)
+            feature_fusion_final = self.feedForwardLayerNorm(feature_fusion + feature_fusionFF)
+            feature_fusion_final = feature_fusion_final.squeeze(-1)
+            feature_fusion_final = feature_fusion_final.transpose(0, 1)
+
+            # funny score
+            output_funny_score = self.FunnyScorelinear1(feature_fusion_final).squeeze(-1)
+            output_funny_score = self.FunnyScorelinear2(output_funny_score).squeeze(-1)
+
+            # gemma generate
+            output_text = self.gemmaGenerate(feature_fusion_final).to(torch.bfloat16)
+            # last_hidden_state = self.gemmaGenerate(feature_fusion)
+            # output_text = self.gemmaLm_head(last_hidden_state)
+
+            # avg_pool = nn.AdaptiveAvgPool1d(768)
+            # text = avg_pool(output_text)
+            text = output_text.transpose(0, 1)
+
+            # Transformer
+            for i in range(depth):
+                # self attention
+                image, text = self.layers_self_multi[i](image, text)
+                # co-attention
+                image, text = self.layers_co_attention[i](image, text)
 
                 # feature fusion
                 feature_fusion = image + text  # visual_attending_textual + textual_attending_visual
-                feature_fusion = self.feedForwardLinear(feature_fusion)
-                feature_fusion = self.feedForwardLayerNorm(feature_fusion + feature_fusion)
-                feature_fusion = feature_fusion.squeeze(-1)
-                feature_fusion = feature_fusion.transpose(0, 1)
+                feature_fusionFF = self.feedForwardLinear(feature_fusion)
+                feature_fusion_final = self.feedForwardLayerNorm(feature_fusion + feature_fusionFF)
+                feature_fusion_final = feature_fusion_final.squeeze(-1)
+                feature_fusion_final = feature_fusion_final.transpose(0, 1)
 
-                # gemma generate
-                last_hidden_state = self.gemmaGenerate(feature_fusion)
-                output_text = self.gemmaLm_head(last_hidden_state)
+            # funny score
+            output_funny_score = self.FunnyScorelinear1(feature_fusion_final).squeeze(-1)
+            output_funny_score = self.FunnyScorelinear2(output_funny_score).squeeze(-1)
 
-                # funny score
-                output_funny_score = self.FunnyScorelinear1(feature_fusion).squeeze(-1)
-                output_funny_score = self.FunnyScorelinear2(output_funny_score).squeeze(-1)
-
-                if lastTurn:  # show final funny score
-                    return generated_caption, output_funny_score
-                else:
-                    next_token_logits = output_text[:, -1, :]
-                    next_token_probs = torch.softmax(next_token_logits, dim=-1)
-                    next_token_id = torch.argmax(next_token_probs, dim=-1).item()
-                    generated_tokens.append(next_token_id)
-
-                    generated_caption = insert_zeros(generated_tokens)
-                    generated_caption = tokenizer.decode(generated_caption, skip_special_tokens=False)
-                    generated_caption = generated_caption.replace("<pad>", " ").replace("  ", " ").split()
-                    generated_caption = [word for word in generated_caption if word[0] != "<"]
-                    generated_caption = " ".join(generated_caption)
-
-                    text = textExtraction(tokenizer, gemmaConfig, [generated_caption]).to(device).to(torch.bfloat16)
-                    text = text.transpose(0, 1)
-
-                    if next_token_id in gemmaConfig.eos_token_id or len(generated_caption.split()) > max_length:
-                        # <eos> = 1; <end_of_turn> = 107
-                        lastTurn = True
+        return output_text, output_funny_score
 
 
 class Discriminator(nn.Module):
@@ -240,20 +259,12 @@ class Discriminator(nn.Module):
         self.g_unc_mlp2 = nn.Linear(64, 1)
         # Discriminator
         self.d_linearFake = nn.Linear(gemmaConfig.vocab_size, 768)
-        self.d_con_mlp1_r2f = nn.Linear(196608, 2)
-        self.d_con_mlp2_r2f = nn.Linear(batch_size, 1)
-        self.d_con_mlp1_f2r = nn.Linear(196608, 2)
-        self.d_con_mlp2_f2r = nn.Linear(batch_size, 1)
-        self.d_con_mlp1_g = nn.Linear(1536, 2)
-        self.d_con_mlp2_g = nn.Linear(64, 1)
-        self.d_con_mlp1_m = nn.Linear(1536, 2)
-        self.d_con_mlp2_m = nn.Linear(64, 1)
-        self.d_unc_mlp1_r = nn.Linear(768, 1)
-        self.d_unc_mlp2_r = nn.Linear(64, 1)
-        self.d_unc_mlp1_g = nn.Linear(768, 1)
-        self.d_unc_mlp2_g = nn.Linear(64, 1)
-        self.d_unc_mlp1_m = nn.Linear(768, 1)
-        self.d_unc_mlp2_m = nn.Linear(64, 1)
+        self.d_con_mlp1_cd = nn.Linear(3072, 2)
+        self.d_con_mlp2_cd = nn.Linear(64, 1)
+        self.d_con_mlp1 = nn.Linear(1536, 2)
+        self.d_con_mlp2 = nn.Linear(64, 1)
+        self.d_unc_mlp1 = nn.Linear(768, 1)
+        self.d_unc_mlp2 = nn.Linear(64, 1)
 
     def forward(self, real_text, fake_text, image, GorD):
         # real_text = [batch_size, 64, 768]
@@ -282,31 +293,34 @@ class Discriminator(nn.Module):
             cd_C_g = C_g.unsqueeze(0).expand(C_g.shape[0], -1, -1, -1)
             d_C_r2f = torch.cat((cd_C_r, cd_C_g.transpose(0, 1)), dim=-1)
             d_C_f2r = torch.cat((cd_C_g, cd_C_r.transpose(0, 1)), dim=-1)
-            d_C_r2f = d_C_r2f.view(d_C_r2f.shape[0], d_C_r2f.shape[1], -1)
-            d_C_f2r = d_C_f2r.view(d_C_f2r.shape[0], d_C_f2r.shape[1], -1)
 
             ######################## conditional ########################
-            d_C_r2f = self.d_con_mlp1_r2f(d_C_r2f)
-            d_C_f2r = self.d_con_mlp1_f2r(d_C_f2r)
-            d_C_g = self.d_con_mlp1_g(C_g)
-            d_C_m = self.d_con_mlp1_m(C_m)
+            d_C_r2f = self.d_con_mlp1_cd(d_C_r2f)
+            d_C_f2r = self.d_con_mlp1_cd(d_C_f2r)
+            d_C_g = self.d_con_mlp1(C_g)
+            d_C_m = self.d_con_mlp1(C_m)
 
-            d_C_r2f = self.d_con_mlp2_r2f(d_C_r2f.transpose(1, 2)).squeeze(-1).unsqueeze(0)
-            d_C_f2r = self.d_con_mlp2_r2f(d_C_f2r.transpose(1, 2)).squeeze(-1).unsqueeze(0)
-            d_C_g = self.d_con_mlp2_g(d_C_g.transpose(1, 2)).squeeze(-1).unsqueeze(0)
-            d_C_m = self.d_con_mlp2_m(d_C_m.transpose(1, 2)).squeeze(-1).unsqueeze(0)
+            d_C_r2f = self.d_con_mlp2_cd(d_C_r2f.transpose(2, 3)).squeeze(-1).unsqueeze(0)
+            d_C_f2r = self.d_con_mlp2_cd(d_C_f2r.transpose(2, 3)).squeeze(-1).unsqueeze(0)
+            d_C_g = self.d_con_mlp2(d_C_g.transpose(1, 2)).squeeze(-1).unsqueeze(0)
+            d_C_m = self.d_con_mlp2(d_C_m.transpose(1, 2)).squeeze(-1).unsqueeze(0)
+
+            d_C_r2f = nn.LogSoftmax(dim=-1)(d_C_r2f)
+            d_C_f2r = nn.LogSoftmax(dim=-1)(d_C_f2r)
+            d_C_r2f = torch.mean(d_C_r2f, dim=-2)
+            d_C_f2r = torch.mean(d_C_f2r, dim=-2)
 
             d_con_output = torch.cat((d_C_r2f, d_C_f2r, d_C_g, d_C_m), dim=0)
             ###############################################################
 
             ######################## unconditional ########################
-            d_UC_r = self.d_unc_mlp1_r(real_text).squeeze(-1)
-            d_UC_g = self.d_unc_mlp1_g(fake_text).squeeze(-1)
-            d_UC_m = self.d_unc_mlp1_m(mismatched_text).squeeze(-1)
+            d_UC_r = self.d_unc_mlp1(real_text).squeeze(-1)
+            d_UC_g = self.d_unc_mlp1(fake_text).squeeze(-1)
+            d_UC_m = self.d_unc_mlp1(mismatched_text).squeeze(-1)
 
-            d_UC_r = self.d_unc_mlp2_r(d_UC_r).squeeze(-1).unsqueeze(0)
-            d_UC_g = self.d_unc_mlp2_g(d_UC_g).squeeze(-1).unsqueeze(0)
-            d_UC_m = self.d_unc_mlp2_m(d_UC_m).squeeze(-1).unsqueeze(0)
+            d_UC_r = self.d_unc_mlp2(d_UC_r).squeeze(-1).unsqueeze(0)
+            d_UC_g = self.d_unc_mlp2(d_UC_g).squeeze(-1).unsqueeze(0)
+            d_UC_m = self.d_unc_mlp2(d_UC_m).squeeze(-1).unsqueeze(0)
 
             d_unc_output = torch.cat((d_UC_r, d_UC_g, d_UC_m), dim=0)
             ###############################################################
@@ -316,11 +330,14 @@ class Discriminator(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #load model
 NetG = Generator().to(torch.bfloat16).to(device)
-NetD = Discriminator().to(torch.bfloat16).to(device)
-checkpoint_G = torch.load('./Model/20241029_1e-6/20241029_1e-6_NetG_5.pth')
-checkpoint_D = torch.load('./Model/20241029_1e-6/20241029_1e-6_NetD_5.pth')
+# NetD = Discriminator().to(torch.bfloat16).to(device)
+
+checkpoint_name = '20241112_gemma_generate_base_20241110_gemma_generate_onlyd'
+checkpoint_G = torch.load('D:/MemeGAN/Model/' + checkpoint_name + '/' + checkpoint_name + '_NetG_11.pth')
+# checkpoint_D = torch.load('D:/MemeGAN/Model/' + checkpoint_name + '/' + checkpoint_name + '_NetD_4.pth')
+
 NetG.load_state_dict(checkpoint_G['model_state_dict'])
-NetD.load_state_dict(checkpoint_D['model_state_dict'])
+# NetD.load_state_dict(checkpoint_D['model_state_dict'])
 # optimizer_G.load_state_dict(checkpoint_G['optimizer_state_dict'])
 # optimizer_D.load_state_dict(checkpoint_D['optimizer_state_dict'])
 # train_losses_FC.append(checkpoint_G['FC_loss'])
@@ -329,10 +346,13 @@ NetD.load_state_dict(checkpoint_D['model_state_dict'])
 # present_epoch = checkpoint_G['epoch'] + 1
 # train with load model
 NetG.train()
-NetD.train()
+# NetD.train()
 # generate
 NetG.eval()
-NetD.eval()
+# NetD.eval()
+test_gt = ['you all loved it so i brought it back']
 image = imageExtraction("./test_img.jpg")
-output = NetG.generate(image.to(device).to(torch.bfloat16), 200)
-print(output)
+text= textExtraction(tokenizer, gemmaConfig, test_gt)
+logits, funnyscore = NetG.generate(text.to(device),image.to(device).to(torch.bfloat16))
+# NetD(image.to(device).to(torch.bfloat16), logits, image.to(device).to(torch.bfloat16), "G")
+print(funnyscore)
