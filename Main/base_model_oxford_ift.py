2734eb2e4f1cd1b4d4713a1ebbebb9f0927a12f2
unknown <iamamanda021024@gmaul.com>
2024-11-21 21:32:34 +0800

    feat: ift share weight

--- Main/base_model_oxford_ift.py
+++ Main/base_model_oxford_ift.py
@@ -35,14 +35,15 @@ class OxfordDataset(torch.utils.data.Dataset):
 
 def train():
     checkpoint = False
-    load_name = '20241110_gemma_generate_onlyd'
+    load_name = 'none'
     load_num = 0
     epochs = 30
-    batch_size = 40
+    batch_size = 128
     optimizer_F_lr = 1e-5
-    save_name = 'ift_test'
+    save_name = '20241121_ift_test'
     if not os.path.exists('./Model/' + save_name):
         os.makedirs('./Model/' + save_name)
+        os.makedirs('D:/MemeGAN/Model/' + save_name)
 
 
     # if args.img - dir == 'Oxford_HIC':
@@ -71,16 +72,16 @@ def train():
     test_dataset = OxfordDataset(test_text, test_image, test_funny_score)
     # train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=20)
     # test_loader = DataLoader(test_dataset, batch_size=128, shuffle=True, num_workers=20)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True, drop_last=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True, drop_last=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=20, pin_memory=True, drop_last=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=20, pin_memory=True, drop_last=True)
 
     ### 官方的Gemma #########################################################################################
     # 2b = 2304, 9b = 3584, 27b = 4608
-    gemma_hiddenstate_size = 2304
+    # gemma_hiddenstate_size = 2304
     tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b-it")
     gemmaConfig = AutoConfig.from_pretrained('google/gemma-2-2b-it')
     ### gemma float32 / bfloat16
-    gemma = AutoModelForCausalLM.from_pretrained("google/gemma-2-2b-it", device_map="auto", torch_dtype=torch.bfloat16)
+    # gemma = AutoModelForCausalLM.from_pretrained("google/gemma-2-2b-it", device_map="auto", torch_dtype=torch.bfloat16)
     ### Local gemma
     # gemma = LocalGemma2ForCausalLM.from_pretrained("google/gemma-2-2b-it", preset="auto", torch_dtype=torch.bfloat16)
     ### gemma int4 / int8
