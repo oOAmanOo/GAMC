49fae59a4fd2c038bce705d9894926240273a948
oOAmanOo_left <iamamanda021024@gmail.com>
2025-01-16 17:25:14 +0800

    feat: swin added

--- Main/oxford_train_BLEU.py
+++ Main/oxford_train_BLEU.py
@@ -400,7 +400,11 @@ class Transformer(nn.Module):
 class TransformerMapper(nn.Module):
 
     def forward(self, x):
+        ### clip ###
         x = self.linear(x).view(x.shape[0], self.clip_length, -1)
+        ### swin ###
+        # x = self.linear(x)
+        ############
         prefix = self.prefix_const.unsqueeze(0).expand(x.shape[0], *self.prefix_const.shape)
         prefix = torch.cat((x, prefix), dim=1)
         out = self.transformer(prefix)[:, self.clip_length:]
@@ -410,7 +414,11 @@ class TransformerMapper(nn.Module):
         super(TransformerMapper, self).__init__()
         self.clip_length = clip_length
         self.transformer = Transformer(dim_embedding, 8, num_layers)
+        ### clip ###
         self.linear = nn.Linear(dim_clip, clip_length * dim_embedding)
+        ### swin ###
+        # self.linear = nn.Linear(768, dim_embedding)
+        ############
         self.prefix_const = nn.Parameter(torch.randn(prefix_length, dim_embedding), requires_grad=True)
 
 class ClipCaptionModel(nn.Module):
