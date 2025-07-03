7958e23f1e5f7c20aebe2f702823c81dcfa00b55
oOAmanOo_left <iamamanda021024@gmail.com>
2025-07-04 07:40:09 +0800

    feat: BITA code

---
+++ Citations/BITA/BITA/BITA/scripts/bita/eval/eval_caption.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.run --nproc_per_node=1 evaluate.py --cfg-path /mnt/c/Users/user/PycharmProjects/BITA/BITA/BITA/BITA/project/bita/eval/caption_eval.yaml
